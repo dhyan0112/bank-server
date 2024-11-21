094bc4cb10e9c8b2a228c24c5f4a5115d208418e
dhyan0112 <dhyanchandsahoo@gmail.com>
2024-11-21 09:44:06 +0530

    bank3

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 
 module.exports = {
-  mongoURI: process.env.MONGO_URI || 'mongodb://localhost:27017/bankingApp',
+  mongoURI: process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/bankingApp',
   JWT_SECRET: process.env.JWT_SECRET || 'your-secret-key',
 };
 
